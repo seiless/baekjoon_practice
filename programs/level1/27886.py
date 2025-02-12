f471fd158bfa918461a6c2e48cc7eb0b56367906
Seiles <you@example.com>
2025-02-12 17:21:19 +0900

    추가문제

---
+++ programs/level1/27886.py
@@ -0,0 +1,6 @@
+# 단어 S와 정수 i가 주어졌을 때,S의i번째 글자를 출력하는 프로그램을 작성하시오.
+
+word = input()
+i = int(input())
+
+print(word[i-1])
---
+++ programs/level1/2920.py
@@ -0,0 +1,2 @@
+# 1부터 8까지 차례대로 연주한다면 ascending, 
+# 8부터 1까지 차례대로 연주한다면 descending, 둘 다 아니라면 mixed
